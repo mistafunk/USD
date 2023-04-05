7e2bdd2190fa4873f68deeed3adbbe86f2f50ca1
Simon Haegler <shaegler@esri.com>
2023-04-05 23:16:17 +0200

    wip: initial prt adaptor

---
+++ third_party/cityengine/adaptor/PrtPrimAdaptor.cpp
@@ -0,0 +1,5 @@
+//
+// Created by shaegler on 4/5/23.
+//
+
+#include "PrtPrimAdaptor.h"
---
+++ third_party/cityengine/adaptor/PrtPrimAdaptor.h
@@ -0,0 +1,94 @@
+#pragma once
+
+#include "pxr/pxr.h"
+#include "pxr/usdImaging/usdImaging/api.h"
+#include "pxr/usdImaging/usdImaging/primAdapter.h"
+#include "pxr/usdImaging/usdImaging/gprimAdapter.h"
+
+PXR_NAMESPACE_OPEN_SCOPE
+
+class PrtRuleAssignment;
+
+class PrtPrimAdaptor : public UsdImagingGprimAdapter {
+public:
+    typedef UsdImagingGprimAdapter BaseAdapter;
+
+    PrtPrimAdaptor() : UsdImagingGprimAdapter() {
+    }
+
+    USDIMAGING_API
+    virtual ~PrtPrimAdaptor() = delete;
+
+    // ---------------------------------------------------------------------- //
+    /// \name Scene Index Support
+    // ---------------------------------------------------------------------- //
+
+    USDIMAGING_API
+    TfTokenVector GetImagingSubprims(UsdPrim const& prim) override;
+
+    USDIMAGING_API
+    TfToken GetImagingSubprimType(
+            UsdPrim const& prim,
+            TfToken const& subprim) override;
+
+    USDIMAGING_API
+    HdContainerDataSourceHandle GetImagingSubprimData(
+            UsdPrim const& prim,
+            TfToken const& subprim,
+            const UsdImagingDataSourceStageGlobals &stageGlobals) override;
+
+    USDIMAGING_API
+    HdDataSourceLocatorSet InvalidateImagingSubprim(
+            UsdPrim const& prim,
+            TfToken const& subprim,
+            TfTokenVector const& properties) override;
+
+    // ---------------------------------------------------------------------- //
+    /// \name Initialization
+    // ---------------------------------------------------------------------- //
+
+    USDIMAGING_API
+    SdfPath Populate(
+        UsdPrim const& prim,
+        UsdImagingIndexProxy* index,
+        UsdImagingInstancerContext const* instancerContext = nullptr) override;
+
+    USDIMAGING_API
+    bool IsSupported(UsdImagingIndexProxy const* index) const override;
+
+    USDIMAGING_API
+    HdDirtyBits ProcessPropertyChange(UsdPrim const& prim,
+                                      SdfPath const& cachePath,
+                                      TfToken const& propertyName) override;
+
+    // ---------------------------------------------------------------------- //
+    /// \name Parallel Setup and Resolve
+    // ---------------------------------------------------------------------- //
+
+    /// Thread Safe.
+    USDIMAGING_API
+    void TrackVariability(
+        UsdPrim const& prim,
+        SdfPath const& cachePath,
+        HdDirtyBits* timeVaryingBits,
+        UsdImagingInstancerContext const* instancerContext = nullptr)
+            const override;
+
+    // ---------------------------------------------------------------------- //
+    /// \name Data access
+    // ---------------------------------------------------------------------- //
+
+    USDIMAGING_API
+    VtValue GetTopology(UsdPrim const& prim,
+                        SdfPath const& cachePath,
+                        UsdTimeCode time) const override;
+
+    // Override the implemetation in GprimAdapter since we don't fetch the
+    // points attribute for implicit primitives.
+    USDIMAGING_API
+    VtValue GetPoints(
+        UsdPrim const& prim,
+        UsdTimeCode time) const override;
+};
+
+PXR_NAMESPACE_CLOSE_SCOPE
