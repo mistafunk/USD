7d99ddafa7efefd0766ec42edf69b43adba6c2e1
Simon Haegler <shaegler@esri.com>
2025-12-08 23:25:33 +0100

    cmake: suppress tbb task.h deprecation warnings
    reduce noise in build output

--- cmake/macros/Private.cmake
+++ cmake/macros/Private.cmake
@@ -1311,6 +1311,7 @@ function(_pxr_library NAME)
     target_compile_definitions(${NAME}
         PUBLIC
             ${apiPublic}
+            TBB_SUPPRESS_DEPRECATED_MESSAGES=1
         PRIVATE
             MFB_PACKAGE_NAME=${PXR_PACKAGE}
             MFB_ALT_PACKAGE_NAME=${PXR_PACKAGE}
